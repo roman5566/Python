dc35b262d8170a680b9c073562124065bb09df4d
Ranbir Aulakh <ranbir2010@live.com>
2013-04-13 18:38:55 -0400

    Added Temperature!

---
+++ Temperatue.py
@@ -0,0 +1,8 @@
+#Convert Fahrenheit to Celius
+
+temp = raw_input("What is the temperature now: ")
+c = (5.0/9.0) * (int(temp) - 32)
+print "Celius is " + str(c)
+print "Fahrenheit is: " + str(temp)
+
+raw_input("Press ENTER to exit")
